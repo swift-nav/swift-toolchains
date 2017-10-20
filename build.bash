79a0759e919a937b6a74f4e1e445522b70a4aa1a
Jason Mobarak <jason@swiftnav.com>
2017-10-19 17:50:31 -0700

    Remove x86 only build

--- build.bash
+++ build.bash
@@ -8,7 +8,6 @@ mkdir -p "build-x86"
 mkdir -p output/opt
 
 MAKE_PACKAGES=
-BUILD_X86=n
 VERBOSE=
 
 while [[ $# -gt 0 ]]; do
@@ -16,11 +15,6 @@ while [[ $# -gt 0 ]]; do
     --make-packages)
       echo "*** Making packages ***"
       MAKE_PACKAGES=y
-      shift
-    ;;
-    --build-x86)
-      echo "*** Building x86_64 (in addition to ARM) ***"
-      BUILD_X86=y
       shift
     ;;
     --verbose)
@@ -42,19 +36,6 @@ CMAKE_COMMAND="\
         -DLLVM_BINUTILS_INCDIR=/usr/include \
         -DLLVM_INCLUDE_TESTS=OFF"
 
-CMAKE_COMMAND_X86="\
-    cmake -G Ninja \
-        /work/obfuscator-llvm \
-        -DCMAKE_INSTALL_PREFIX=/opt/llvm-obfuscator \
-        -DCMAKE_CXX_FLAGS='-DENDIAN_LITTLE=1' \
-        -DCMAKE_C_COMPILER=/usr/bin/gcc \
-        -DCMAKE_CXX_COMPILER=/bin/cpp_wrapper.py \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DLLVM_TARGET_ARCH=X86 \
-        -DLLVM_TARGETS_TO_BUILD=X86 \
-        -DLLVM_BINUTILS_INCDIR=/usr/include \
-        -DLLVM_INCLUDE_TESTS=OFF"
-
 PATCH_COMMAND="git apply /patches/*.patch"
 
 docker run -i -t --rm \
@@ -73,28 +54,4 @@ if [[ -n "$MAKE_PACKAGES" ]]; then
 
   ./stage_sysroot.bash
   tar -C output -cJf "llvm-obfuscator-arm-x86.txz" .
-fi
-
-if [[ -n "$BUILD_X86" ]]; then
-
-  docker run -i -t --rm \
-    -v $PWD/output/opt:/opt \
-    arm-llvm-obf:base \
-    /bin/bash -c "rm -rvf /opt/*"
-
-  docker run -i -t --rm \
-      -v "$PWD/build-x86:/work/build" \
-      -v $PWD/output/opt:/opt \
-      -v $PWD/patches:/patches \
-      arm-llvm-obf:base \
-      /bin/bash -c "cd /work/obfuscator-llvm \
-                    && $PATCH_COMMAND \
-                    && cd /work/build \
-                    && $CMAKE_COMMAND_X86 \
-                    && ninja $VERBOSE \
-                    && ninja $VERBOSE install"
-
-  if [[ -n "$MAKE_PACKAGES" ]]; then
-    tar -C output -cJf "llvm-obfuscator-x86.txz" .
-  fi
 fi
