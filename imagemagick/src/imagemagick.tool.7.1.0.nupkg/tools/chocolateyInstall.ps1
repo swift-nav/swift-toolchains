94ab974ade8fa3b4c08f974406e80ab82448cf44
Jason Mobarak <jason@swiftnav.com>
2022-06-01 13:47:55 -0700

    Add custom ImageMagick package

---
+++ imagemagick/src/imagemagick.tool.7.1.0.nupkg/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageArgs = @{
+  packageName    = 'imagemagick.tool'
+  url            = 'https://github.com/swift-nav/swift-toolchains/releases/download/imagemagick-7.1.0/ImageMagick-7.1.0-portable-Q16-x86.zip'
+  url64          = 'https://github.com/swift-nav/swift-toolchains/releases/download/imagemagick-7.1.0/ImageMagick-7.1.0-portable-Q16-x64.zip'
+  fallbackUrl    = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-portable-Q16-x86.zip'
+  fallbackUrl64  = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-portable-Q16-x64.zip'
+  checksum       = '2CCCD00CD8E904FA749F13B40F5D22121C82395053DC07CC6327C9B408F8D513'
+  checksum64     = 'E41595EBCB17267F0E9F52AEDB2B2DA37B0EBDEAB6722D15DF28949910E82E8E'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+}
+
+try {
+    Get-WebHeaders $packageArgs.url
+}
+catch {
+    $packageArgs.url = $packageArgs.fallbackUrl
+    $packageArgs.url64 = $packageArgs.fallbackUrl64
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ imagemagick/src/makepkg.ps1
@@ -0,0 +1,2 @@
+cd imagemagick.tool.7.1.0.nupkg
+7z -tzip a ..\..\imagemagick.tool.7.1.0.nupkg .
