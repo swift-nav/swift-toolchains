4b71fd6c34a7794cbd31e337bfd08568bffc4580
Isaac Torres <isaac@isaactorz.dev>
2023-04-22 19:18:12 -0600

    syntax fix

--- jenkins/createArmhfMuslGcc.groovy
+++ jenkins/createArmhfMuslGcc.groovy
@@ -2,7 +2,7 @@ def call(jenkins) {
     String name = 'armhf-musl-gcc'
     Map options = [
         dockerFile: 'docker/Dockerfile',
-    ],
+    ]
     Closure action = {
         sh """/bin/bash -ex
         echo "hello world!"
